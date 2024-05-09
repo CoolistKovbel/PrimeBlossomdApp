1d1c4d536c3e263420c6eb7b8d0a5306fb0a9b05
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-05-09 19:41:19 +0300

    slow

--- app/components/landing/Token.tsx
+++ app/components/landing/Token.tsx
@@ -35,7 +35,7 @@ const Token = () => {
 
           <div className="w-full text-left font-bold ">
             <p className="p-2">Total Supply: 1,000,000</p>
-            <p className="p-2">Current value: 1 eth - 1000 SLT</p>
+            <p className="p-2">Current value: 1 eth - 1000 PBT</p>
           </div>
         </div>
         
