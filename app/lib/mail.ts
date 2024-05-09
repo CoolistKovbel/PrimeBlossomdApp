459780c10ec6a11853819c9867c05c1f7b7a4b4c
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-05-09 04:16:28 +0300

    slowly

--- app/lib/mail.ts
+++ app/lib/mail.ts
@@ -1,4 +1,4 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
 interface SendMailProps {
   to: string;
@@ -7,38 +7,38 @@ interface SendMailProps {
   content: string;
 } 
 
-// export async function sendMail({ to, name, subject, content }: SendMailProps) {
-//   const { SMTP_PASSWORD, SMTP_EMAIL } = process.env;
+export async function sendMail({ to, name, subject, content }: SendMailProps) {
+  const { SMTP_PASSWORD, SMTP_EMAIL } = process.env;
 
-//   const transport = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: SMTP_EMAIL,
-//       pass: SMTP_PASSWORD,
-//     },
-//   });
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+  });
 
-//   try {
+  try {
 
-//     const test = await transport.verify();
-//     console.log(test, 'there is transpeort');
+    const test = await transport.verify();
+    console.log(test, 'there is transpeort');
 
 
-//   } catch (error) {
-//     console.log(error);
-//     return;
-//   }
+  } catch (error) {
+    console.log(error);
+    return;
+  }
 
-//   try {
-//     const result = await transport.sendMail({
-//       from: SMTP_EMAIL,
-//       to,
-//       subject,
-//       html: content,
-//     });
+  try {
+    const result = await transport.sendMail({
+      from: SMTP_EMAIL,
+      to,
+      subject,
+      html: content,
+    });
 
-//     console.log(result);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
